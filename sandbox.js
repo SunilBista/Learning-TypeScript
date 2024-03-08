da69709f6638a7b17c00c10a99b3d8000ae08368
SunilBista <sunilbista303@gmail.com>
2024-03-08 11:34:45 +1100

    feat: learn explicit type

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,27 @@
-var character = "mario";
-console.log(character);
-console.log("luigi");
-var inputs = document.querySelectorAll("input");
-console.log("inputs", inputs);
-inputs.forEach(function (input) {
-    console.log("input", input);
-});
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log("circle", circ(12));
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+//age = 'one'; //not correct
+age = 26;
+isLoggedIn = true;
+character = "sunil";
+//arrays
+var newArray = []; //initialization to an empty array to append
+newArray.push("sunil");
+//union types (mixed array)
+var mixedArray = [];
+mixedArray.push(true);
+mixedArray.push("bista");
+mixedArray.push(26);
+console.log("mixed", mixedArray);
+//union type for normal variable
+var uid; //no parenthesis required
+uid = "onetwothree";
+uid = 123;
+//objects
+var newObject;
+newObject = { name: "sunil", age: 26 };
+newObject = []; //since array is an object
+//moreSpecific object declaration
+var moreSpecificObject;
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,49 @@
-const character = "mario";
+//explicit types
 
-console.log(character);
-console.log("luigi");
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-const inputs = document.querySelectorAll("input");
+//age = 'one'; //not correct
 
-console.log("inputs", inputs);
+age = 26;
+isLoggedIn = true;
+character = "sunil";
 
-inputs.forEach((input) => {
-  console.log("input", input);
-});
+//arrays
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let newArray: string[] = []; //initialization to an empty array to append
+
+newArray.push("sunil");
+
+//union types (mixed array)
+
+let mixedArray: (string | number | boolean)[] = [];
+
+mixedArray.push(true);
+mixedArray.push("bista");
+mixedArray.push(26);
+
+console.log("mixed", mixedArray);
+
+//union type for normal variable
+
+let uid: string | number; //no parenthesis required
+
+uid = "onetwothree";
+uid = 123;
+
+//objects
+let newObject: object;
+
+newObject = { name: "sunil", age: 26 };
+
+newObject = []; //since array is an object
+
+//moreSpecific object declaration
+
+let moreSpecificObject: {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
 };
-
-console.log("circle", circ(12));
