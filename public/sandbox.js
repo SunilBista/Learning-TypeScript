47da541f6f42d00eb3bea13cbcc8ee148a940f09
SunilBista <sunilbista303@gmail.com>
2024-03-08 12:37:39 +1100

    feat: learn function signature

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,27 @@
 "use strict";
 console.log("hello");
+//https://www.typescriptlang.org/docs/
+//example one
+let handleSum; //this is a signature
+handleSum = (a, b) => {
+    //it should match the signature
+    console.log("The sum is ", a + b);
+};
+//example two
+let calc;
+calc = (a, b, c) => {
+    if ((c = "sum")) {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+console.log(calc(5, 7, "sum"));
+//example three
+let logDetails;
+logDetails = (personObj) => {
+    console.log(`${personObj === null || personObj === void 0 ? void 0 : personObj.name} is ${personObj === null || personObj === void 0 ? void 0 : personObj.age} years old`);
+};
+let passObject = { name: "Sunil", age: 26 };
+logDetails(passObject);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,35 @@
 console.log("hello");
 
 //https://www.typescriptlang.org/docs/
+
+//example one
+
+let handleSum: (a: number, b: number) => void; //this is a signature
+
+handleSum = (a: number, b: number) => {
+  //it should match the signature
+  console.log("The sum is ", a + b);
+};
+//example two
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (a: number, b: number, c: string) => {
+  if ((c = "sum")) {
+    return a + b;
+  } else {
+    return a - b;
+  }
+};
+
+console.log(calc(5, 7, "sum"));
+
+//example three
+
+let logDetails: (obj: { name: string; age: number }) => void;
+
+logDetails = (personObj: { name: string; age: number }) => {
+  console.log(`${personObj?.name} is ${personObj?.age} years old`);
+};
+
+logDetails({ name: "Sunil", age: 26 });
