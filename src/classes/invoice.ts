8a22a53233b64e12656c6c4c9d61e8d8235795b1
SunilBista <sunilbista303@gmail.com>
2024-03-13 00:17:38 +1100

    feat: learn modules

---
+++ src/classes/invoice.ts
@@ -0,0 +1,25 @@
+// classes
+// class Invoice {
+//   client: string;
+//   details: string;
+//   amount: number;
+
+//   constructor(c: string, d: string, a: number) {
+//     this.client = c;
+//     this.details = d;
+//     this.amount = a;
+//   }
+//   format() {
+//     return `${this.client} owes ${this.amount} for ${this.details}`;
+//   }
+// }
+export class Invoice {
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  }
+}
